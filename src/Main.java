b6dba31a99c59358832b0a2a51752ad5c41d81cc
Your Name <you@example.com>
2024-11-29 10:08:17 +0100

    init

--- src/Main.java
+++ src/Main.java
@@ -9,46 +9,53 @@ public class Main {
     public static void main(String[] args) throws IOException {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        String name = "Nadja";
-        final String DATEN_SATZ = "lALELEU";
-        int age = 33;
-        double happyNumber = 10.5;
-        boolean isGameOver = false;
-        char character = 'a';
-        char character2 = 'b';
-        int resultChars = character + character2;
-        System.out.println(resultChars);
+//        String name = "Nadja";
+//        final String DATEN_SATZ = "lALELEU";
+//        int age = 33;
+//        double happyNumber = 10.5;
+//        boolean isGameOver = false;
+//        char character = 'a';
+//        char character2 = 'b';
+//        int resultChars = character + character2;
+//        System.out.println(resultChars);
+//
+//
+//        //System.out.printf("Hello and welcome!");
+//        System.out.println("Hallo" + name + "djhgfkjhdgkj");
+//        System.out.println("lerjlerkl");
+//        System.out.println("test " + (char) resultChars);
+//
+//
+//        BufferedReader reader = new BufferedReader(
+//                new InputStreamReader(System.in));
+//        System.out.printf("Wie heißt Du?\n");
+//        name = reader.readLine();
+//        System.out.printf("Wie alt bist Du?\n");
+//        age = Integer.parseInt(reader.readLine());
+//        System.out.printf("Was ist Deine Glückszahl?\n");
+//        happyNumber = Double.parseDouble(reader.readLine());
+//        System.out.printf("Hallo %s. ", name);
+//        System.out.printf("Du bist %s Jahre alt. ", age);
+//        System.out.printf("Deine Glückszahl ist %s: ", happyNumber);
 
-
-        //System.out.printf("Hello and welcome!");
-        System.out.println("Hallo" + name + "djhgfkjhdgkj");
-        System.out.println("lerjlerkl");
-        System.out.println("test " + (char) resultChars);
-
-
-        /*BufferedReader reader = new BufferedReader(
-                new InputStreamReader(System.in));
-        System.out.printf("Wie heißt Du?\n");
-        name = reader.readLine();
-        System.out.printf("Wie alt bist Du?\n");
-        age = Integer.parseInt(reader.readLine());
-        System.out.printf("Was ist Deine Glückszahl?\n");
-        happyNumber = Double.parseDouble(reader.readLine());
-        System.out.printf("Hallo %s. ", name);
-        System.out.printf("Du bist %s Jahre alt. ", age);
-        System.out.printf("Deine Glückszahl ist %s: ", happyNumber);
-*/
         // SCANNER
 
+//        Scanner s = new Scanner(System.in);
+//        name = s.nextLine();
+//        age = s.nextInt();
+//        happyNumber = s.nextDouble();
+//        //System.out.printf("Hallo " + name + ". Du bist " + age + " Jahre alt. Deine Glückszahl ist " + happyNumber);
+//        System.out.printf("Hallo %s, Du bist %d Jahre alt und Deine Glückszahl ist %f.1", name, age, happyNumber);
+
         Scanner s = new Scanner(System.in);
-        name = s.nextLine();
-        age = s.nextInt();
-        happyNumber = s.nextDouble();
-        //System.out.printf("Hallo " + name + ". Du bist " + age + " Jahre alt. Deine Glückszahl ist " + happyNumber);
-        System.out.printf("Hallo %s, Du bist %d Jahre alt und Deine Glückszahl ist %f.1", name, age, happyNumber);
+        System.out.printf("Matrix von 0 bis 99:\n");
+        int numberMatrix = s.nextInt();
+        System.out.printf("Zahl von 2 bis 9:\n");
+        int numberZahl = s.nextInt();
+        for (int i = 0; i < numberMatrix; i++) {
+            System.out.printf("%d %d", i, i+1);
+        }
+
+
     }
-
-
-
-
 }
